8bcb6ecda45ac92a953a89cb133c4223da97ea00
Ermi539 <ermiashabtamu477@gmail.com>
2024-06-30 17:10:58 +0300

    basic_operations.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 gcodinh
+h
---
+++ python_introductions/basic_operations.py
@@ -0,0 +1,7 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Mertek/AppData/Local/Programs/Python/Python312/basic_operations.py
+Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
