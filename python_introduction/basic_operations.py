5d26b563ca518e7e54bddbcdd60a34745e170b08
Ermi539 <ermiashabtamu477@gmail.com>
2024-06-30 17:39:17 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,8 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Mertek/AppData/Local/Programs/Python/Python312/basic_operations.py
-Addition of 10 and 5 is 15
-Subtraction of 10 and 5 is 5
-Multiplication of 10 and 5 is 50
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
